04be4a1e4ad9541c10e68456f2641fba2a6ff5b0
franku2005 <francomondacaaiii@gmail.com>
2024-07-23 10:59:59 -0300

    utilizamos directivas ngIf para el navegador e instalamos sweetAlert2

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/modules/autentificacion/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  logueado = true; //booleana para manejar el resgistro y el inicio de sesion
+  deslogueado = false; //booleana para manejo de cierre de sesión
+  constructor(
+    public servicioAuth: AuthService,
+    public servicioRutas: Router
+  ) {}
+
+  //Funcion "ingresar" para invertir los valores
+  ingresar(){
+    this.logueado=false;
+    this.deslogueado =true;
+  }
+  cerrarSesion(){
+    this.deslogueado =false;
+    this.logueado = true; 
+
+    this.servicioAuth.CerrarSesion();
+    this.servicioRutas.navigate(['/'])
+  }
 
 }
